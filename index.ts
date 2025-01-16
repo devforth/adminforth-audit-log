3805ff81e6e0fb961cf3526da87f1cd946cc414c
Ivan Borshchov <ivan@devforth.io>
2025-01-16 23:26:21 +0000

    fix(autorelease): do test fix

--- index.ts
+++ index.ts
@@ -10,8 +10,6 @@ import utc from 'dayjs/plugin/utc.js';
 import { AdminForthPlugin, AllowedActionsEnum, AdminForthSortDirections, AdminForthDataTypes, HttpExtra } from "adminforth";
 import { PluginOptions } from "./types.js";
 
-a.1 = 2;
-
 dayjs.extend(utc);
 
 export default class AuditLogPlugin extends AdminForthPlugin {
